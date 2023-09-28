0659fa6e4e09aa2411afd921ea08807c9b87a7e4
André Luiz <alps2@cin.ufpe.br>
2023-09-28 13:06:17 +0000

    original query

--- sql scripts/corregodoria_optimization.sql
+++ sql scripts/corregodoria_optimization.sql
@@ -1,3 +1,33 @@
+-- Original query
+SELECT
+proc."NPU",
+proc."processoID",
+anon_1.earliest_date,
+c."descricao" AS "classe",
+a."descricao" AS "assunto",
+mov."activity",
+mov."dataInicio",
+mov."dataFinal",
+mov."usuarioID",
+mov."duration",
+COALESCE(mov."movimentoID", -1) AS "movimentoID",
+COALESCE(s."nomeServidor", 'N/I') AS "nomeServidor",
+COALESCE(s."tipoServidor", 'N/I') AS "tipoServidor"
+FROM test_processos_xxxx proc
+JOIN classes c ON proc.classe = c.id
+JOIN assuntos a ON proc.assunto = a.id
+JOIN test_movimentos_xxxx mov ON proc."processoID" = mov."processoID"
+LEFT OUTER JOIN servidores s ON mov."usuarioID" = s."servidorID"
+LEFT OUTER JOIN cod_movimentos cm ON mov."movimentoID" = cm.id
+JOIN (
+    SELECT
+    mov."processoID" AS "processoID",
+    min(mov."dataInicio") AS earliest_date
+    FROM test_movimentos_xxxx mov
+    GROUP BY mov."processoID"
+) AS anon_1 ON proc."processoID" = anon_1."processoID"
+WHERE anon_1.earliest_date >= '1900-01-01'
+ORDER BY proc."processoID", mov."dataFinal" ASC;
 
 -- Creating index on dataPrimeiroMovimento
 CREATE INDEX idx_dataPrimeiroMovimento
